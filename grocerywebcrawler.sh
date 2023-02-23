17056ef73659a6220108105d5093e24293abad7e
Jimmy Chen <jimmy@Jimmys-MBP.MG8702>
2023-02-22 17:10:26 -0800

    changing some files

--- grocerywebcrawler.sh
+++ grocerywebcrawler.sh
@@ -1,6 +1,3 @@
 echo "starting daily cron scripts"
-cd /cronjob
-python3 -m venv venv
-pip install --no-cache-dir --upgrade -r /cronjob/requirements.txt
 python3 ./webcrawl.py
 python3 ./pricechange.py
--- increment_counter.sh
+++ increment_counter.sh
@@ -1,5 +1,2 @@
 echo "incrementing counter"
-cd /cronjob
-python3 -m venv venv
-pip install --no-cache-dir --upgrade -r /cronjob/requirements.txt
 python3 ./counter.py
