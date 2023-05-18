de76f98fc03eb305cc0d4befacdeb70e3a036c4a
Jimmy Chen <jimmy@jimmys-mbp.lan>
2023-05-18 17:08:05 -0600

    fixing bug

--- src/webcrawl2887.py
+++ src/webcrawl2887.py
@@ -2,7 +2,7 @@ from grocerywebcrawler.safeway_crawler import get_all_safeway_items_from_store
 
 
 def webcrawl():
-    get_all_safeway_items_from_store(705)
+    get_all_safeway_items_from_store(2887)
 
 
 if __name__ == '__main__':
